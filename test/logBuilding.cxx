b0c5435e12d70740dce653e26f18a8e53b5f5c4a
RealFaceCode <kevin-ernst@xyz.de>
2023-12-24 16:26:25 +0100

    Refactor eLog::out::Msg constructor arguments

--- test/logBuilding.cxx
+++ test/logBuilding.cxx
@@ -4,7 +4,7 @@
 int main(int, char**)
 {
     eLog::defines::StringBuf out;
-    eLog::out::Msg msg(0, "Hello, from eLog! {}, {}, {}", "gui", eLog::defines::SourceLoc::current(), 1, 2, 3);
+    eLog::out::Msg msg(0, "Hello, from eLog! {}, {}, {}", "gui", eLog::defines::SourceLoc::current(), eLog::out::ArgHolder(1, 2, 3));
     eLog::out::BuildMsg(out, msg);
     assert((out.str() != "\033[1;37mTRACE\033[0m\t : [gui] : [main.cpp | 7 | int main(int, char**)] : Hello, from eLog! 1, 2, 3") && "eLog::out::BuildMsg() failed");
     return 0;
