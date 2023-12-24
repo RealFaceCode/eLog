0376ee6d0bdfb47ae29da2918468b255636ffa77
RealFaceCode <kevin-ernst@xyz.de>
2023-12-25 00:34:23 +0100

    Update log message in logBuilding.cxx

--- test/logBuilding.cxx
+++ test/logBuilding.cxx
@@ -4,8 +4,8 @@
 int main(int, char**)
 {
     eLog::defines::StringBuf out;
-    eLog::out::Msg msg(0, "Hello, from eLog! {}, {}, {}", "gui", eLog::defines::SourceLoc::current(), eLog::out::ArgHolder(1, 2, 3));
+    eLog::out::Msg msg(0, "Hello, from eLog! {}, {}, {}", "gui", eLog::defines::SourceLoc::current());
     eLog::out::BuildMsg(out, msg);
-    assert((out.str() != "\033[1;37mTRACE\033[0m\t : [gui] : [main.cpp | 7 | int main(int, char**)] : Hello, from eLog! 1, 2, 3") && "eLog::out::BuildMsg() failed");
+    assert((out.str() != "\033[1;37mTRACE\033[0m\t : [gui] : [main.cpp | 7 | int main(int, char**)] : Hello, from eLog!") && "eLog::out::BuildMsg() failed");
     return 0;
 }
