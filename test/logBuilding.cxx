7cc68675eaf11454b03e6e848d7b3b71a6b4d621
RealFaceCode <kevin-ernst@xyz.de>
2023-12-25 02:01:27 +0100

    Refactor logBuilding.cxx to use formatted string in eLog::out::Msg constructor

--- test/logBuilding.cxx
+++ test/logBuilding.cxx
@@ -4,7 +4,7 @@
 int main(int, char**)
 {
     eLog::defines::StringBuf out;
-    eLog::out::Msg msg(0, {"Hello, from eLog! {}, {}, {}"}, "gui", "test", 23, 3.4f);
+    eLog::out::Msg msg(0, "Hello, from eLog! {}, {}, {}", "gui", "test", 23, 3.4f);
     eLog::out::BuildMsg(out, msg);
     assert((out.str() != "\033[1;37mTRACE\033[0m\t : [gui] : [main.cpp | 7 | int main(int, char**)] : Hello, from eLog!") && "eLog::out::BuildMsg() failed");
     return 0;
