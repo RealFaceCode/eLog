6642c5f37e3b5704c7befd8aec49892ab888897d
RealFaceCode <kevin-ernst@xyz.de>
2024-01-04 23:34:51 +0100

    Fix precision issue in fmtPrecision.cxx

--- test/fmtPrecision.cxx
+++ test/fmtPrecision.cxx
@@ -11,16 +11,16 @@ void test_precision()
     ::assert(Format("{:.2}", 3.151), "3.15");
 
     ::assert(Format("{:.2}", 3.1415f), "3.14");
-    ::assert(Format("{:.2}", 3.145f), "3.15");
+    ::assert(Format("{:.2}", 3.145f), "3.14");
     ::assert(Format("{:.2}", 3.149f), "3.15");
     ::assert(Format("{:.2}", 3.150f), "3.15");
     ::assert(Format("{:.2}", 3.151f), "3.15");
 
     ::assert(Format("{:.5}", 3.1415), "3.14150");
-    ::assert(Format("{:.5}", 3.145l), "3.14500");
-    ::assert(Format("{:.5}", 3.149l), "3.14900");
-    ::assert(Format("{:.5}", 3.150l), "3.15000");
-    ::assert(Format("{:.5}", 3.151l), "3.15100");
+    ::assert(Format("{:.5}", 3.145), "3.14500");
+    ::assert(Format("{:.5}", 3.149), "3.14900");
+    ::assert(Format("{:.5}", 3.150), "3.15000");
+    ::assert(Format("{:.5}", 3.151), "3.15100");
 
     ::assert(Format("{:.1}", 3.1415), "3.1");
     ::assert(Format("{:.1}", 3.145), "3.1");
