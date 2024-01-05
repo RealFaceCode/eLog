7e5d3010b506fc4daf00469e07df753fae2ddc28
RealFaceCode <kevin-ernst@xyz.de>
2024-01-05 16:25:42 +0100

    Update alignment in fmtAlignment.cxx

--- test/fmtAlignment.cxx
+++ test/fmtAlignment.cxx
@@ -4,13 +4,13 @@ using namespace tmp::fmt;
 
 void testAlignment()
 {
-    ::assert(Format("{<4}", 42), "42  ");
-    ::assert(Format("{>4}", 42), "  42");
-    ::assert(Format("{^<4}", 42), " 42 ");
+    ::assert(Format("|{<4}|", 42), "|42  |");
+    ::assert(Format("|{>4}|", 42), "|  42|");
+    ::assert(Format("|{^<4}|", 42), "| 42 |");
 
-    ::assert(Format("{:<10}", 3.1415), "3.1415    ");
-    ::assert(Format("{:>10}", 3.1415), "    3.1415");
-    ::assert(Format("{:^<10}", 3.1415), "  3.1415  ");
+    ::assert(Format("|{:<10}|", 3.1415f), "|3.1415    |");
+    ::assert(Format("|{:>10}|", 3.1415f), "|    3.1415|");
+    ::assert(Format("|{:^<10}|", 3.1415f), "|  3.1415  |");
 }
 
 int main()
