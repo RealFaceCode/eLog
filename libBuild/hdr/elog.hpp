bfbdea791ff3c8f6bd1ee4e147ca8d0ebc6e2e80
RealFaceCode <kevin-ernst@xyz.de>
2024-01-07 19:12:35 +0100

    Add custom logging functions
    Description: This commit adds custom logging functions to the `elog` namespace. These functions allow for logging messages with different log levels such as TRACE, DEBUG, INFO, WARNING, ERROR, and FATAL. The functions also support formatting of log messages using `fmt::FormatString`. The new functions `Trace`, `Debug`, `Info`, `Warn`, `Error`, and `Fatal` are added to the `elog` namespace. These functions internally use the `LogCustom` function to log messages.

--- libBuild/hdr/elog.hpp
+++ libBuild/hdr/elog.hpp
@@ -3,15 +3,12 @@
 #include <vector>
 #include <string>
 #include <string_view>
+#include "state.hpp"
+#include "thread.hpp"
+#include "msg.hpp"
 
 namespace elog
 {
-    namespace structs
-    {
-        template<typename... Args>
-        struct Msg;
-    }
-
     namespace internal
     {
         std::vector<std::stringbuf>& GetBuffer();
@@ -21,4 +18,49 @@ namespace elog
     void SetLogFilePath(std::string_view path);
     void CloseFileStream();
 
+    template<typename... Params>
+    inline void LogCustom(std::string_view logLevel, std::string_view label, fmt::FormatString format, Params&&... params)
+    {
+        structs::Msg<> msg(logLevel, label, std::move(format), std::forward<Params>(params)...);
+        if(internal::IsFlagSet(enums::StateFlag::LOG_THREAD))
+            internal::AddTask(std::move(msg));
+        else
+            internal::logOut(std::move(msg));
+    }
+
+    template<typename... Params>
+    inline void Trace(std::string_view label, fmt::FormatString format, Params&&... params)
+    {
+        LogCustom("TRACE", label, std::move(format), std::forward<Params>(params)...);
+    }
+
+    template<typename... Params>
+    inline void Debug(std::string_view label, fmt::FormatString format, Params&&... params)
+    {
+        LogCustom("DEBUG", label, std::move(format), std::forward<Params>(params)...);
+    }
+
+    template<typename... Params>
+    inline void Info(std::string_view label, fmt::FormatString format, Params&&... params)
+    {
+        LogCustom("INFO", label, std::move(format), std::forward<Params>(params)...);
+    }
+
+    template<typename... Params>
+    inline void Warn(std::string_view label, fmt::FormatString format, Params&&... params)
+    {
+        LogCustom("WARNING", label, std::move(format), std::forward<Params>(params)...);
+    }
+
+    template<typename... Params>
+    inline void Error(std::string_view label, fmt::FormatString format, Params&&... params)
+    {
+        LogCustom("ERROR", label, std::move(format), std::forward<Params>(params)...);
+    }
+
+    template<typename... Params>
+    inline void Fatal(std::string_view label, fmt::FormatString format, Params&&... params)
+    {
+        LogCustom("FATAL", label, std::move(format), std::forward<Params>(params)...);
+    }
 }
