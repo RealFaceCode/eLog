7ff9fda377a6ddc2a641d987d79afc665d3238f1
RealFaceCode <kevin-ernst@xyz.de>
2024-01-07 18:46:19 +0100

    Add log file functionality and set log file path

--- libBuild/hdr/elog.hpp
+++ libBuild/hdr/elog.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include <string_view>
+
+namespace elog
+{
+    namespace structs
+    {
+        template<typename... Args>
+        struct Msg;
+    }
+
+    namespace internal
+    {
+        std::vector<std::stringbuf>& GetBuffer();
+        void logOut(structs::Msg<>&& msg);
+    }
+
+    void SetLogFilePath(std::string_view path);
+    void CloseFileStream();
+
+}
