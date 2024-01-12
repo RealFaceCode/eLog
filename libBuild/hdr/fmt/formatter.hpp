863ef15138ee46f2e71b991041546101f1fd4480
RealFaceCode <kevin-ernst@xyz.de>
2024-01-12 18:05:38 +0100

    Add support for unordered_map and vector in Formatter class

--- libBuild/hdr/fmt/formatter.hpp
+++ libBuild/hdr/fmt/formatter.hpp
@@ -2,8 +2,10 @@
 #include <sstream>
 #include <string>
 #include <ostream>
+#include <unordered_map>
+#include <vector>
 
-namespace elog::formatMap
+namespace elog::fmt
 {
     template<typename T>
     struct Formatter
@@ -21,25 +23,35 @@ namespace elog::formatMap
     {
         static std::string format(const std::vector<T>& value)
         {
-            std::ostringstream ss;
-            ss << "[";
+            std::stringbuf buf;
+            buf.sputc('[');
+            std::string str;
             for (auto& item : value)
             {
-                ss << Formatter<T>::format(item) << ", ";
+                if(&item == &value.back())
+                    str = Formatter<T>::format(item);
+                else
+                    str = Formatter<T>::format(item) + ", ";
+                buf.sputn(str.c_str(), str.size());
             }
-            ss << "]";
-            return std::move(ss.str());
+            buf.sputc(']');
+            return std::move(buf.str());
         }
     };
-
     template<typename T, typename U>
     struct Formatter<std::pair<T, U>>
     {
         static std::string format(const std::pair<T, U>& value)
         {
-            std::ostringstream ss;
-            ss << "(" << Formatter<T>::format(value.first) << ", " << Formatter<U>::format(value.second) << ")";
-            return std::move(ss.str());
+            std::stringbuf buf;
+            buf.sputn("[First: ", 8);
+            auto str = Formatter<T>::format(value.first);
+            buf.sputn(str.c_str(), str.size());
+            buf.sputn(", Second: ", 10);
+            str = Formatter<U>::format(value.second);
+            buf.sputn(str.c_str(), str.size());
+            buf.sputc(']');
+            return std::move(buf.str());
         }
     };
 
@@ -48,16 +60,14 @@ namespace elog::formatMap
     {
         static std::string format(const std::unordered_map<T, U>& value)
         {
-            std::ostringstream ss;
-            ss << "{";
+            std::stringbuf buf;
+            buf.sputc('{');
+            std::string str;
             for (auto& item : value)
-            {
-                ss << Formatter<std::pair<T, U>>(item) << ", ";
-            }
-            ss << "}";
-            return std::move(ss.str());
+                str = Formatter<std::pair<T, U>>::format(item);
+            buf.sputc('}');
+            return std::move(buf.str());
         }
     };
-
 }
 
