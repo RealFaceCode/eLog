2a93f562294674b247b156209239563e49b6d9f6
RealFaceCode <kevin-ernst@xyz.de>
2024-01-12 18:52:23 +0100

    Add formatter.hpp include and update parseArg function signature

--- libBuild/hdr/fmt/argparser.hpp
+++ libBuild/hdr/fmt/argparser.hpp
@@ -3,6 +3,7 @@
 #include "checks.hpp"
 #include "argument.hpp"
 #include "exceptions.hpp"
+#include "formatter.hpp"
 
 #include <cstring>
 
@@ -20,11 +21,8 @@ namespace elog::fmt
         std::vector<Argument>& getArgList();
 
     private:
-        template <typename Arg>
-        void argToString(std::stringbuf& ss, Argument& argument, Arg& arg);
-
         template<typename Arg>
-        void parseArg(Arg& arg);
+        void parseArg(const Arg& arg);
 
         template<typename... Args>
         void parseArgs(Args&& ...args);
@@ -43,108 +41,11 @@ namespace elog::fmt
         return mArguments;
     }
 
-    template <typename Arg>
-    inline void ArgParser::argToString(std::stringbuf& buf, Argument& argument, Arg& arg)
-    {
-        std::stringstream ss;
-        if constexpr (is_string_type_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::String);
-            if constexpr (is_cstring_v<not_ref_type<Arg>>)
-            {
-                buf.sputn(arg, strlen(arg));
-            }
-            else
-            {
-                buf.sputn(arg.data(), arg.size());
-            }
-        }
-        else if constexpr (is_integer_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::Int);
-            ss << arg;
-            auto view = ss.view();
-            buf.sputn(view.data(), view.size());
-        }
-        else if constexpr (is_float_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::Float);
-            ss << arg;
-            auto view = ss.view();
-            buf.sputn(view.data(), view.size());
-        }
-        else if constexpr (is_double_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::Double);
-            ss << arg;
-            auto view = ss.view();
-            buf.sputn(view.data(), view.size());
-        }
-        else if constexpr (is_bool_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::Bool);
-            ss << arg;
-            auto view = ss.view();
-            buf.sputn(view.data(), view.size());
-        }
-        else if constexpr (is_pointer_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::Pointer);
-            ss << (uintptr_t)arg;
-            auto view = ss.view();
-            buf.sputn(view.data(), view.size());
-        }
-        else if constexpr (is_container_v<not_ref_type<Arg>> && !is_string_type_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::Container);
-
-            buf.sputc('[');
-            for(auto& element : arg)
-            {
-                if constexpr (is_container_v<decltype(element)> || is_pair_v<decltype(element)>)
-                {
-                    argToString(buf, argument, element);
-                    if(element != arg.back())
-                        buf.sputn(", ", 2);
-                }
-                else
-                {
-                    if(element != arg.back())
-                    {
-                        argToString(buf, argument, element);
-                        buf.sputn(", ", 2);
-                    }
-                    else
-                        argToString(buf, argument, element);
-                }
-            }
-            buf.sputc(']');
-        }
-        else if constexpr (is_pair_v<not_ref_type<Arg>>)
-        {
-            argument.setArgumentType(ArgumentType::Container);
-
-            buf.sputn("[First: ", 8);
-            argToString(buf, argument, arg.first);
-            buf.sputn(", Second: ", 10);
-            argToString(buf, argument, arg.second);
-            buf.sputc(']');	
-        }
-        else
-        {
-            throw ArgumentException("Argument type not supported");
-        }
-    }
-
     template<typename Arg>
-    inline void ArgParser::parseArg(Arg& arg)
+    inline void ArgParser::parseArg(const Arg& arg)
     {
         Argument argument;
-        std::stringbuf buf;
-        
-        argToString(buf, argument, arg);
-
-        argument.value = std::move(buf);
+        Formatter<Arg>::format_to(argument.value, arg);
         mArguments.push_back(std::move(argument));
     }
 
